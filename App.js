2a59acae9c341f63c75bd5243bf08e62796901f6
Varun Sharma <61429136+varunsharma21@users.noreply.github.com>
2023-08-07 17:05:42 +0530

    [UPDATE] : used useEffect() hook
    - Using useEffect() hook in order to make a request immediately when App.js component runs for the very first time without clicking the button.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -6,22 +6,57 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  async function fetchMovieHandler() {
+  const fetchMovieHandler = useCallback(async () => {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
+    setError(null); // so that previous error does not show up.
 
-    setMovies(transformedMovies);
-    setIsLoading(false);
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+
+      if (!response.ok) {
+        throw Error("Something went wrong :/");
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+
+      setMovies(transformedMovies);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false); // Setting isLoading to false no matter we found error or not.
+  }, []);
+
+  // Using useEffect in order to make a request immediately when App.js component
+  // runs for the very first time without clicking the button.
+  // Although we can also press the button to send request.
+  useEffect(() => {
+    fetchMovieHandler();
+  }, [fetchMovieHandler]);
+
+  let content = <h2>No movies to show.</h2>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (isLoading) {
+    content = <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    content = <h2>{error}</h2>;
   }
 
   return (
@@ -29,11 +64,7 @@ function App() {
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <h2>No movies to show.</h2>}
-        {isLoading && <h2>Loading...</h2>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
